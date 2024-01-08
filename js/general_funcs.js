a0f9129cd6ca452cf16d18d20d44795ce1fdef2a
ntistephens <neilthorne87@gmail.com>
2024-01-08 12:52:41 -0500

    Update general_funcs.js

--- js/general_funcs.js
+++ js/general_funcs.js
@@ -31,15 +31,6 @@ function setQuestionFirstSecondThirdPlace(item, questionDetails) {
   }
 }
 
-
-// function setQuestionFirstOrSecondPlace(item, questionDetails) {
-//   if(questionDetails.question_pick_counter == 1){
-//     questionDetails.first_place_item = item;
-//   } else if(questionDetails.question_pick_counter == 2){
-//     questionDetails.second_place_item = item;
-//   }
-// }
-
 function setQuestionLastPlaces(item_group, questionDetails) {
   for (let i = 0; i < item_group.length; i++) {
     if((item_group[i].wasSel == false) && questionDetails.fourth_place_item == null){
@@ -52,24 +43,13 @@ function setQuestionLastPlaces(item_group, questionDetails) {
   }
 }
 
-
-// function setQuestionThirdPlace(item_group, questionDetails) {
-//   for (let i = 0; i < item_group.length; i++) {
-//     if(item_group[i].wasSel == false){
-//       questionDetails.third_place_item = item_group[i];
-//       setWasSel(item_group[i]);
-//       break;
-//     }
-//   }
-// }
-
 function setWasSel(item){
   item.wasSel = true;
 }
 
 function displayTestResult(questionDetails, el){
-  let resultText = "1st: " + questionDetails.first_place_item.name + ", " + "2nd: " + questionDetails.second_place_item.name + ", " + "3rd: " + questionDetails.third_place_item.name + " (Last Places: " + questionDetails.fourth_place_item.name + ", " + questionDetails.fifth_place_item.name + ")";
-  $(el).text(resultText);
+  let resultText = "1st: " + questionDetails.first_place_item.name + "<br>" + "2nd: " + questionDetails.second_place_item.name + "<br>" + "3rd: " + questionDetails.third_place_item.name + "<br>" + "(Last Places: " + questionDetails.fourth_place_item.name + ", " + questionDetails.fifth_place_item.name + ")";
+  $(el).html(resultText);
 }
 
 
@@ -88,43 +68,18 @@ function resetWasSelOnQuestionItems(questionItems) {
   }
 }
 
-
-function set_question_groups_for_round_two_or_three() {
-  sortByTestRank(all_items);
-  var top_tier = [];
-  var middle_tier = [];
-  var bottom_tier = [];
-
-  // place all items into proper tier
-  for (let i = 0; i < all_items.length; i++) {
-    if(i <= 29) {
-      top_tier.push(all_items[i]);
-    } else if (i <= 59) {
-      middle_tier.push(all_items[i]);
-    } else {
-      bottom_tier.push(all_items[i]);
-    }
-  }
-
-  shuffle(top_tier);
-  shuffle(middle_tier);
-  shuffle(bottom_tier);
-
-  push_question_groups_into_round(top_tier);
-  push_question_groups_into_round(middle_tier);
-  push_question_groups_into_round(bottom_tier);
-
+function set_question_groups_for_round(items_ary){
+  shuffle(items_ary);
+  push_question_groups_into_round(items_ary);
   update_seen_items(round.question_groups);
-
   shuffle(round.question_groups);
 }
 
-
-
-
-
 function populateQuestionButtons(round) {
   $('.test-question-btn').each(function(index){
+    console.log("index: " + index);
+    console.log("question group index: " + round.question_group_index);
+    console.log(round);
     $(this).html(round.question_groups[round.question_group_index][index].name);
     $(this).attr("data-id", round.question_groups[round.question_group_index][index].id);
   });
@@ -154,45 +109,212 @@ function update_value_rankings_display(){
   $('#valueRankingsDisplay').find('tbody').empty();
   sortByTestRank(all_items);
   for (let i in all_items) {
-      $('#valueRankingsDisplay').find('tbody').append("<tr><td>" + all_items[i].name + "</td><td>" + all_items[i].weightedVal + "</td></tr>");
+      $('#valueRankingsDisplay').find('tbody').append("<tr><td>" + all_items[i].name + "</td><td>" + all_items[i].weightedVal + "</td><td>" +  all_items[i].removed +  "</td><td>" + display_round_removed(all_items[i]) + "</td></tr>");
   }
 }
 
-
-
+function display_round_removed(item) {
+  if(item.round_removed == null){
+     return " "
+  } else {
+    return item.round_removed
+  }
+}
 
 function processDisplayQuestionResults(){
   $('#test-question-prompt').addClass("d-none");
   $('#test-question-result').removeClass('d-none');
   displayTestResult(questionDetails, $('#test-question-result-data'));
   $('#test-question-continue').removeClass('d-none');
-  determineContinueDisplayAtQuestionResults();
+  $('#test-question-continue').removeClass('d-none');
 }
 
-function determineContinueDisplayAtQuestionResults(){
- // Within rounds 1-3 AND context is not after the 30th question
- if ( ((round.round_counter == 1) || (round.round_counter == 2) (round.round_counter == 3)) && (round.question_group_index != 18) ) {
-   $('#test-question-continue').removeClass('d-none');
- }
-}
+
 
 function round_is_complete(){
-  if((round.round_counter == 1 || round.round_counter == 2 || round.round_counter == 3) && (round.question_group_index == 29)){
+  if((round.round_counter == 1 || round.round_counter == 2 || round.round_counter == 3) && (round.question_group_index == 18)){
+    return true;
+  } else if((round.round_counter == 4) && round.question_group_index == 11) {
+    return true;
+  } else if((round.round_counter == 5) && round.question_group_index == 7) {
+    return true;
+  } else if((round.round_counter == 6) && round.question_group_index == 4) {
     return true;
   }
   return false;
 }
 
-function populate_question_groups_for_next_round(){
-  if((round.round_counter == 2 || round.round_counter == 3) && (round.question_group_index == 29)){
-    set_question_groups_for_round_two_or_three();
+// There are 6 total rounds.  If the counter says Round 7: then the 6 rounds are all complete.
+function all_rounds_are_completed() {
+  if(round.round_counter == 7) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// This function assumes it is being called at the end of a round
+// It removes the hardcoded amount of items, given what round has just ended
+// As long as it isn't the end of Round 6 (becuase end of round 6 is the end of the entire test) It also popuulates the question groups for the next round
+function processQuestionsForEndOfRound(){
+  console.log("Working on processQuestionsForEndOfRound");
+  console.log(all_items);
+  if(round.round_counter == 1){
+    console.log("processQuestionsForEndOfRound: it is round 1");
+    // This is simple: No state updating for items removed
+    set_question_groups_for_round(all_items);
+  } else if(round.round_counter == 2) {
+    console.log("processQuestionsForEndOfRound: it is round 2");
+    // This is simple: No state updating for items removed
+    set_question_groups_for_round(all_items);
+  } else if(round.round_counter == 3) {
+    // Must set removed field on item to true, and also set round_removed field
+    // Ideally: remove 40%, for 90 items that would be removing 36 items
+    // For ease of the 5-item question groups: removing 35 items (55 items remaining)
+    sortByTestRank(all_items);
+    for(let item_index in all_items) {
+      if(item_index >= 55) {
+        all_items[item_index].removed = true;
+        all_items[item_index].round_removed = round.round_counter;
+      }
+    }
+
+    var items_after_removal = []
+    for(let item_index in all_items) {
+      if(all_items[item_index].removed == false){
+        items_after_removal.push(all_items[item_index]);
+      }
+    }
+    set_question_groups_for_round(items_after_removal);
+
+  } else if(round.round_counter == 4) {
+    // Must set removed field on item to true, and also set round_removed field
+    // Ideally: remove 40%, for 55 remaining items that would be removing 22 items
+    // For ease of the 5-item question groups: removing 20 items (35 items remaining)
+    var remaining_items = [];
+    for(let item_index in all_items) {
+      if(all_items[item_index].removed == false){
+        remaining_items.push(all_items[item_index]);
+      }
+    }
+    sortByTestRank(remaining_items);
+    for(let item_index in remaining_items) {
+      if(item_index >= 35) {
+        remaining_items[item_index].removed = true;
+        remaining_items[item_index].round_removed = round.round_counter;
+      }
+    }
+
+    var items_after_removal = []
+    for(let item_index in all_items) {
+      if(all_items[item_index].removed == false){
+        items_after_removal.push(all_items[item_index]);
+      }
+    }
+    set_question_groups_for_round(items_after_removal);
+  } else if(round.round_counter == 5) {
+    // Must set removed field on item to true, and also set round_removed field
+    // Ideally: remove 40%, for 35 remaining items that would be removing 14 items
+    // For ease of the 5-item question groups: removing 15 items (20 items remaining)
+    var remaining_items = [];
+    for(let item_index in all_items) {
+      if(all_items[item_index].removed == false){
+        remaining_items.push(all_items[item_index]);
+      }
+    }
+    sortByTestRank(remaining_items);
+    for(let item_index in remaining_items) {
+      if(item_index >= 20) {
+        remaining_items[item_index].removed = true;
+        remaining_items[item_index].round_removed = round.round_counter;
+      }
+    }
+
+    var items_after_removal = []
+    for(let item_index in all_items) {
+      if(all_items[item_index].removed == false){
+        items_after_removal.push(all_items[item_index]);
+      }
+    }
+    set_question_groups_for_round(items_after_removal);
+  } else if(round.round_counter == 6) {
+    // Must set removed field on item to true, and also set round_removed field
+    // Round 6 We simply round to removing the bottom 10 items to allow the user to focus on the top 10
+    var remaining_items = [];
+    for(let item_index in all_items) {
+      if(all_items[item_index].removed == false){
+        remaining_items.push(all_items[item_index]);
+      }
+    }
+    sortByTestRank(remaining_items);
+    for(let item_index in remaining_items) {
+      if(item_index >= 10) {
+        remaining_items[item_index].removed = true;
+        remaining_items[item_index].round_removed = round.round_counter;
+      }
+    }
+  }
+}
+
+
+// Given the array of Items passed in, it groups them into 5 core values each
+// This function does it's best to group the core values together with ones that have not been compared before.
+function push_question_groups_into_round(ary_of_items){
+  //sanity check: a round should have all the question groups cleared whenever this function is called
+  round.question_groups = []
+
+  // sanity check: init this state tracker for all items
+  for (let i = 0; i < ary_of_items.length; i++) {
+    ary_of_items[i].is_assigned_question_group = false;
+  }
+
+  // For loop creating a question group.
+  for (let i = 0; i < ary_of_items.length; i+=5) {
+    var new_question_group = [];
+
+    // Add the four remaining items to the question group
+    // Priority (done here) is to create unique question groups (meaning: no items have seen each other yet in a test question)
+    for (let ii = 0; ii < ary_of_items.length; ii++) {
+      if( (new_question_group.length < 5) && (ary_of_items[ii].is_assigned_question_group == false) && projected_item_never_seen_by_other_questions_within_group(new_question_group, ary_of_items[ii]) ) {
+          new_question_group.push(ary_of_items[ii]);
+          ary_of_items[ii].is_assigned_question_group = true;
+      } else {
+        for (let iii = 0; iii < ary_of_items.length; iii++) {
+          if( (new_question_group.length < 5) && (ary_of_items[iii].is_assigned_question_group == false) && projected_item_never_seen_by_other_questions_within_group(new_question_group, ary_of_items[iii]) ) {
+            new_question_group.push(ary_of_items[iii]);
+            ary_of_items[iii].is_assigned_question_group = true;
+          }
+        }
+      }
+    }
+
+    // Especially in later rounds, not all question groups can be completely unique (meaning every core value item has not yet been compared to ANY other core value)
+    // Here we ensure that each question group is completed, even if it is not unique
+    if(new_question_group.length < 5){
+      for (let iii = 0; iii < ary_of_items.length; iii++) {
+        if( (new_question_group.length < 5) && (ary_of_items[iii].is_assigned_question_group == false) ) {
+          new_question_group.push(ary_of_items[iii]);
+          ary_of_items[iii].is_assigned_question_group = true;
+        }
+      }
+    }
+
+    round.question_groups.push(new_question_group);
+  } // For loop creating a question group.
+
+  // Now that all the questions are pushed into the round:
+  // we can reset the is_assigned_question_group state tracker
+  for (let i = 0; i < ary_of_items.length; i++) {
+    ary_of_items[i].is_assigned_question_group = false;
   }
 }
 
 function projected_item_never_seen_by_other_questions_within_group(question_group, projected_item_to_add) {
-  for (let item_within_question_group in question_group) {
-    for (let seen_item in item_within_question_group.seen_items) {
-      if(seen_item == projected_item_to_add){
+  // iterate through items without question_group
+  for (let i = 0; i < question_group.length; i++) {
+    // iterate through the seen_items for a particular item within the question_group
+    for (let ii = 0; ii < question_group[i].seen_items.length; ii++) {
+      if(question_group[i].seen_items[ii] == projected_item_to_add){
         return false;
       }
     }
@@ -201,74 +323,12 @@ function projected_item_never_seen_by_other_questions_within_group(question_grou
   return true;
 }
 
-function set_question_groups_for_round_one() {
-  shuffle(all_items);
-  for (let i = 0; i < all_items.length; i+=5) {
-    round.question_groups.push( [all_items[i], all_items[i + 1], all_items[i + 2], all_items[i + 3], all_items[i + 4]] );
-    all_items[i].seen_items.push(all_items[i + 1]);
-    all_items[i].seen_items.push(all_items[i + 2]);
-    all_items[i].seen_items.push(all_items[i + 3]);
-    all_items[i].seen_items.push(all_items[i + 4]);
-    all_items[i + 1].seen_items.push(all_items[i]);
-    all_items[i + 1].seen_items.push(all_items[i + 2]);
-    all_items[i + 1].seen_items.push(all_items[i + 3]);
-    all_items[i + 1].seen_items.push(all_items[i + 4]);
-    all_items[i + 2].seen_items.push(all_items[i]);
-    all_items[i + 2].seen_items.push(all_items[i + 1]);
-    all_items[i + 2].seen_items.push(all_items[i + 3]);
-    all_items[i + 2].seen_items.push(all_items[i + 4]);
-    all_items[i + 3].seen_items.push(all_items[i]);
-    all_items[i + 3].seen_items.push(all_items[i + 1]);
-    all_items[i + 3].seen_items.push(all_items[i + 2]);
-    all_items[i + 3].seen_items.push(all_items[i + 4]);
-    all_items[i + 4].seen_items.push(all_items[i]);
-    all_items[i + 4].seen_items.push(all_items[i + 1]);
-    all_items[i + 4].seen_items.push(all_items[i + 2]);
-    all_items[i + 4].seen_items.push(all_items[i + 3]);
-  }
-}
-
-// Do not use if round 1 because round 1 is simple and uses function: set_question_groups_for_round_one()
-function push_question_groups_into_round(ary_of_items){
-  for (let i = 0; i < ary_of_items.length; i++) {
-    var new_question_group = [];
-    if(ary_of_items[i].is_assigned_question_group == false){
-      new_question_group.push(ary_of_items[i]);
-      ary_of_items[i].is_assigned_question_group = true;
-
-      // Add the two remaining items to the question group
-      // Priority (done here) is to create unique question groups (meaning: no items have seen each other yet in a test question)
-      for (let ii = 0; ii < ary_of_items.length; ii++) {
-        if( (new_question_group.length < 3) && (ary_of_items[ii].is_assigned_question_group == false) && (ary_of_items[i] != ary_of_items[ii]) ) {
-          if(projected_item_never_seen_by_other_questions_within_group(new_question_group, ary_of_items[ii])){
-            new_question_group.push(ary_of_items[ii]);
-            ary_of_items[ii].is_assigned_question_group = true;
-          }
-        }
-      }
-
-      // Especially in later rounds, not all question groups can be unique
-      // Here we ensure that each question group is completed, even if it is not unique
-      if(new_question_group.length < 3){
-        for (let iii = 0; iii < ary_of_items.length; iii++) {
-          if( (ary_of_items[iii].is_assigned_question_group == false) && (ary_of_items[i] != ary_of_items[iii]) ) {
-            new_question_group.push(ary_of_items[iii]);
-            ary_of_items[iii].is_assigned_question_group = true;
-          }
-        }
-      }
-
-    }
-    round.question_groups.push(new_question_group);
-  }
-}
-
 function update_seen_items(round_question_groups) {
-  for (let question_group in round_question_groups) {
-    for (let item in question_group) {
-      for (let item_to_test_seen in question_group) {
-        if( (item != item_to_test_seen) && (item_not_in_group(item_to_test_seen, item.seen_items)) ) {
-           item.seen_items.push(item_to_test_seen);
+  for (let question_group_index in round_question_groups) {
+    for (let item_index in round_question_groups[question_group_index]) {
+      for (let item_to_test_seen_index in round_question_groups[question_group_index]) {
+        if( (round_question_groups[question_group_index][item_index] != round_question_groups[question_group_index][item_to_test_seen_index]) && (item_not_in_group(round_question_groups[question_group_index][item_to_test_seen_index], round_question_groups[question_group_index][item_index].seen_items)) ) {
+           round_question_groups[question_group_index][item_index].seen_items.push(round_question_groups[question_group_index][item_to_test_seen_index]);
         }
       }
     }
